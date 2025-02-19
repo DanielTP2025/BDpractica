07805b5e32377bad38f38535d4301b652a2ae163
DanielT <danielstrabajs@gmail.com>
2025-02-19 11:56:41 -0500

    Salidas

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola Clase");
+
+        System.out.println("Repasando Comandos");
+        System.out.println("Repasando Comandos");
+
+        System.out.println("Suma");
+        System.out.println("Resta");
+        System.out.println("Multiplicacion");
+
+        System.out.println("DDL");
+        System.out.println("DBL");
+        System.out.println("TCL");
+        System.out.println("Comandos Bases de Datos");
     }
 }
